6f3b527e8de6edfe8f129b232fe92f506b0fdea8
David <grayd500@gmail.com>
2024-01-15 00:40:42 -0500

    update with getter code

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,20 +1,33 @@
 import { LightningElement, track } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
+    /***Data binding example */
     fullname="Zero To Hero"
     title ="aura"
-
     changeHandler(event){
         this.title = event.target.value
     }
-
-    address={
+    /***@track binding exmple */
+    @track address={
         city: 'Austin',
         postcode: 78751,
         country: 'USA'
     }
-
     trackHandler(event){
-        this.address= {...this.address, "city":event.target.value} 
+        this.address.city = event.target.value
+    }
+
+    /***getter example */
+    users= ["john", "smith", "nik" ]
+    userFirst = this.users[0]
+    num1 = 10
+    num2 = 20
+
+    get firstUser(){
+        return this.users[0]
+    }
+
+    get multiply(){
+        return this.num1*this.num2
     }
 }
