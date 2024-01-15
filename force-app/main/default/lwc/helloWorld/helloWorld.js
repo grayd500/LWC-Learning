f9bbc9b3789def9fab8d8884ed90623f7c8578cc
David <grayd500@gmail.com>
2024-01-15 00:11:25 -0500

    replace @track with spreading for best practice

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -8,14 +8,13 @@ export default class HelloWorld extends LightningElement {
         this.title = event.target.value
     }
 
-    @track address={
+    address={
         city: 'Austin',
         postcode: 78751,
         country: 'USA'
     }
 
     trackHandler(event){
-        this.address.city = event.target.value
+        this.address= {...this.address, "city":event.target.value} 
     }
-
 }
