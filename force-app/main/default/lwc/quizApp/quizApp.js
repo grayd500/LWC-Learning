827886a79c598b3a3c4675a7634fc5fae94ad900
David <grayd500@gmail.com>
2024-01-16 11:09:52 -0500

    Update with querySelector component

--- force-app/main/default/lwc/quizApp/quizApp.js
+++ force-app/main/default/lwc/quizApp/quizApp.js
@@ -3,6 +3,8 @@ import { LightningElement } from 'lwc';
 export default class QuizApp extends LightningElement {
 
     selected={}  // for storing answers
+    correctAnswers = 0 // to show the number of correct answers
+    isSubmitted = false // use to show the result
     myQuestions=[
         {
             id:"Question1",
@@ -35,22 +37,33 @@ export default class QuizApp extends LightningElement {
             correctAnswer:"c"
         }
     ]
-
+    // used for disabling the sbmit button
     get allNotSelected() {
         return !(Object.keys(this.selected).length === this.myQuestions.length)
     }
+    // for applying dynamic styling to our result
+    get isScoredFull(){
+        return `slds-text-heading_large ${this.myQuestions.length === this.correctAnswers?
+            'slds-text-color_success':'slds-text-color_error'}`
+        }
+        // changeHandler get's called on every click on the options
     changeHandler(event){
         console.log("name", event.target.name)
         console.log("value", event.target.value)
         const {name, value} = event.target
-        // const name = event.target.name
-        // const value = event.target.value     
         this.selected={...this.selected, [name]:value}
     }
-    submitHandler(){
-
+    //form submit handler
+    submitHandler(event){
+        event.preventDefault()
+        let correct = this.myQuestions.filter(item=>this.selected[item.id] === item.correctAnswer)
+        this.correctAnswers = correct.length
+        this.isSubmitted = true
     }
+    //form reset handler
     resetHandler(){
-
+            this.selected ={}
+            this.correctAnswers=0
+            this.isSubmitted = false
+            }
     }
-}
