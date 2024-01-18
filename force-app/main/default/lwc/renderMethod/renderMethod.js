8bd95231d359979af1e405fbdb219c5416b414bb
David <grayd500@gmail.com>
2024-01-18 17:03:23 -0500

    Update render method to show changes on button clicks

--- force-app/main/default/lwc/renderMethod/renderMethod.js
+++ force-app/main/default/lwc/renderMethod/renderMethod.js
@@ -5,9 +5,16 @@ import signinTemplate from './signinTemplate.html'
 import signupTemplate from './signupTemplate.html'
 import renderTemplate from './renderMethod.html'
 export default class RenderMethod extends LightningElement {
-
+    selectedBtn=''
     render(){
-        return renderTemplate
+        return this.selectedBtn === 'Signup' ? signupTemplate :
+                this.selectedBtn === 'Signin' ? signinTemplate :
+                renderTemplate
     }
-
+    handleClick(event){
+        this.selectedBtn = event.target.label
+    }
+    submitHandler(event){
+        console.log(`${event.target.label} Successful!!`)
+    }
 }
