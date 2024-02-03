e4c49dd31b6d1f7babf3171d3a5203525c1e25b5
David <grayd500@gmail.com>
2024-02-03 14:57:52 -0500

    Update with Aura/LWC communication in Components Communication app

---
+++ force-app/main/default/lwc/customLightningCard/customLightningCard.js
@@ -0,0 +1,11 @@
+// LWC Learning2\force-app\main\default\lwc\customLightningCard\customLightningCard.js:
+import { LightningElement } from 'lwc';
+
+export default class CustomLightningCard extends LightningElement {
+    handleSlotFooterChange(){
+        const footerElem = this.template.querySelector('footer')
+        if(footerElem){
+            footerElem.classList.remove('slds-hide')
+        }
+    }
+}
