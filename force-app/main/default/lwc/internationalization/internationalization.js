366f6b23a7990ee4784187587341f618a05333ef
David <grayd500@gmail.com>
2024-02-04 15:17:58 -0500

    Add text to stati label

---
+++ force-app/main/default/lwc/internationalization/internationalization.js
@@ -0,0 +1,14 @@
+import { LightningElement } from 'lwc';
+import LOCALE from '@salesforce/i18n/locale'
+import CURRENCY from '@salesforce/i18n/currency'
+import DIR from '@salesforce/i18n/dir'
+export default class Internationalization extends LightningElement {
+    dir = DIR //'rtl'
+    number = 6575557.86
+    formattedNumber = new Intl.NumberFormat(LOCALE, {
+        style:'currency',
+        currency:CURRENCY,
+        currencyDisplay:'symbol'
+    }).format(this.number)
+
+}
