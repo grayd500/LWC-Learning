f94d8415b9bf7f32df6543e17ebad92e9326d435
David <grayd500@gmail.com>
2024-02-04 13:06:33 -0500

    Update with static library for css animate

--- force-app/main/default/lwc/thirdPartyFiles/thirdPartyFiles.js
+++ force-app/main/default/lwc/thirdPartyFiles/thirdPartyFiles.js
@@ -1,17 +1,27 @@
 import { LightningElement } from 'lwc';
 import MOMENT from '@salesforce/resourceUrl/moment'
-import {loadScript} from 'lightning/platformResourceLoader'
+import ANIMATE from '@salesforce/resourceUrl/animate'
+import {loadScript, loadStyle} from 'lightning/platformResourceLoader'
 export default class ThirdPartyFiles extends LightningElement {
 currentDate=''
+isLibLoaded = false
     renderedCallback(){
-        Promise.all([
-            loadScript(this, MOMENT+'/moment/moment.min.js')
-        ]).then(()=>{
-            //success
-            this.setDateOnScreen()
-        }).catch(error=>{
-            console.error(error)
-        })
+        if(this.isLibLoaded){
+            return
+        } else {
+
+            Promise.all([
+                loadStyle(this, ANIMATE+'/animate/animate.min.css'),
+                loadScript(this, MOMENT+'/moment/moment.min.js')
+            ]).then(()=>{
+                this.setDateOnScreen()
+            })
+                this.isLibLoaded = true
+
+
+                
+        }
+
     }
     setDateOnScreen(){
         this.currentDate = moment().format('LLLL')
