cf8123a8d51c2aa7110562e77619e2ee905cffad
David <grayd500@gmail.com>
2024-02-05 11:27:35 -0500

    Added score panel and some styling for it

--- force-app/main/default/lwc/memoryGameLwc/memoryGameLwc.js
+++ force-app/main/default/lwc/memoryGameLwc/memoryGameLwc.js
@@ -1,3 +1,21 @@
 import { LightningElement } from 'lwc';
+import { loadStyle } from 'lightning/platformResourceLoader';
+import fontawesome from '@salesforce/resourceUrl/fontawesome';
 
-export default class MemoryGameLwc extends LightningElement {}
+export default class MemoryGameLwc extends LightningElement {
+    isLibLoaded = false;
+    renderedCallback() {
+        if (this.isLibLoaded) {
+            return;
+        }
+        loadStyle(this, fontawesome + '/fontawesome/css/font-awesome.min.css')
+            .then(() => {
+                console.log("FontAwesome CSS loaded successfully");
+            })
+            .catch(error => {
+                console.error("Error loading the FontAwesome CSS", error);
+            });
+        this.isLibLoaded = true;
+    }
+}
+
